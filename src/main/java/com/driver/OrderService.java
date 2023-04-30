fc0668f21ab328f82c08bf8aafc143960482ac70
Yusuf <mohammad.yusufkhan12345@gmail.com>
2023-05-01 00:45:27 +0530

    amazoff assignment

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -8,8 +8,7 @@ import java.util.List;
 @Service
 public class OrderService {
 
-    @Autowired
-    OrderRepository orderRepository;
+    OrderRepository orderRepository = new OrderRepository();
 
     public String addOrder(Order order) {
 
